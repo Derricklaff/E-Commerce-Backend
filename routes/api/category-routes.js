c01b700d75a8e40bb0918a466792f5ba0c076020
Derrick Lafforthun <derricklaff@gmail.com>
2022-11-25 10:22:29 -0700

    finished models. Starting on api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,77 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    let data = Category.findAll({
+      include: [{ model: Product }],
+    })
+    res.status(200).json(data);
+  }
+  catch (err) {
+    res.json(err)
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    let data = Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{ model: Product }]
+    });
+
+    if (!data) {
+      return res.status(404).json("Invalid Field");
+    }
+
+    res.status(200).json(data);
+  }
+  catch (err) {
+    res.json(err)
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+//Create a new category
+router.put('/:id', async (req, res) => {
+  try {
+    let data = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    if (!data) {
+      return res.status(404).json("Invalid Field");
+    }
+
+    res.status(200).json("Category updated");
+  }
+  catch (err) {
+    res.json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-});
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+
+router.delete('/:id', async (req, res) => {
+  try {
+    let data = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+
+    if (!data) {
+      return res.status(404).json("Invalid Field");
+    }
+
+    res.status(200).json("Category deleted")
+  }
+  catch (err) {
+    res.json(err)
+  }
 });
 
 module.exports = router;
